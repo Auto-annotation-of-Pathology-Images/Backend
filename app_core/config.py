83a8499c6fbe83bf7e0303447e310c31513c9bc1
HenryNebula <c-huang2015011098@hotmail.com>
2020-12-15 23:19:03 -0500

    fix requirements

--- app_core/config.py
+++ app_core/config.py
@@ -1,9 +1,4 @@
-from os import environ, path
-from pathlib import Path
-from dotenv import load_dotenv
-
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+from os import environ
 
 
 class Config:
